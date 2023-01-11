14573d2b00c48565435e8f0dbc827c5c37290c2d
Abir Bhattacharya <70687014+abirbhattacharya82@users.noreply.github.com>
2023-01-11 04:24:56 +0000

    Checks

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
-
+import axios from 'axios';
 function App() {
+  const options = {
+    method: 'GET',
+    url: 'https://bhagavad-gita3.p.rapidapi.com/v2/chapters/',
+    params: {limit: '18'},
+    headers: {
+      'X-RapidAPI-Key': 'faaf235513msh73cd090780fbee3p11b255jsnc38e3cb87bfe',
+      'X-RapidAPI-Host': 'bhagavad-gita3.p.rapidapi.com'
+    }
+  };
+  axios.request(options).then(function (response) {
+    console.log(response.data);
+  }).catch(function (error) {
+    console.error(error);
+  });
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
     </div>
   );
 }
