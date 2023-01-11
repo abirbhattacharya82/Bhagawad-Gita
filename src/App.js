a317425d7cd3c8613595954fd740034c7a8ddc3a
Abir Bhattacharya <70687014+abirbhattacharya82@users.noreply.github.com>
2023-01-11 16:00:10 +0000

    Styling Left

--- src/App.js
+++ src/App.js
@@ -1,23 +1,8 @@
 import './App.css'
-import axios from 'axios';
 import Header from './Components/Header/Header'
 import Subheader from './Components/Subheader/Subheader'
 import Result from './Components/Result/Result'
 function App() {
-  const options = {
-    method: 'GET',
-    url: 'https://bhagavad-gita3.p.rapidapi.com/v2/chapters/',
-    params: {limit: '18'},
-    headers: {
-      'X-RapidAPI-Key': 'faaf235513msh73cd090780fbee3p11b255jsnc38e3cb87bfe',
-      'X-RapidAPI-Host': 'bhagavad-gita3.p.rapidapi.com'
-    }
-  };
-  axios.request(options).then(function (response) {
-    console.log(response.data);
-  }).catch(function (error) {
-    console.error(error);
-  });
   return (
     <div className="App">
       <Header></Header>
--- src/Components/Page/Page.js
+++ src/Components/Page/Page.js
@@ -1,11 +1,28 @@
 import React from "react";
 import { useState } from "react";
-
+import axios from "axios";
 const Page=(props)=>{
-    console.log(props);
+    console.log(props.prop);
+    const [chapter_summary, setCh]=useState("");
+    const options = {
+        method: 'GET',
+        url: `https://bhagavad-gita3.p.rapidapi.com/v2/chapters/${props.prop}/`,
+        headers: {
+          'X-RapidAPI-Key': 'faaf235513msh73cd090780fbee3p11b255jsnc38e3cb87bfe',
+          'X-RapidAPI-Host': 'bhagavad-gita3.p.rapidapi.com'
+        }
+      };
+      
+      axios.request(options).then(function (response) {
+          console.log(response.data.chapter_summary);
+          setCh(response.data.chapter_summary);
+      }).catch(function (error) {
+          console.error(error);
+      });
+    
     return(
         <div>
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Placeat tempora eius accusamus velit reiciendis ipsum iusto animi. A sunt ipsam incidunt, autem repellat magnam ipsum, nihil sapiente omnis accusamus voluptatibus.
+            {chapter_summary}
         </div>
     )
 };
